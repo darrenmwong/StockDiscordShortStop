b8bcf88b60b9f4df6e56b5f46d7382d585872259
Darren Wong <darrenwong@Darrens-MacBook-Pro.local>
2021-03-14 19:34:59 -0700

    remove comment

--- index.js
+++ index.js
@@ -30,20 +30,6 @@ client.on('message', message => {
         message.reply('Not a real command or error');
     }
 
-    // let ticker = args[1].toUpperCase();
-    // let url = config.payload_url + ticker + "&token=" + config.finnhub_token; 
-
-    // async function getStockPrices() {
-    //     const response = await fetch(url);
-    //     if(response.ok) {
-    //         let json = await response.json();
-    //         message.reply('Current Price: ' + json.c + '. Open Price: ' + json.o);
-    //     } else {
-    //         console.log('error', response);
-    //     }
-    // }
-    // getStockPrices();
-
 });
 
 client.login(config.token);
